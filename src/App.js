7bf362c71baa61ddc7379f0282cc6bb543e0b268
Julianne <juliaguas17@gmail.com>
2023-05-09 20:52:47 -0500

    added pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </Router>
     </>
   );
